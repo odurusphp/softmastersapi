6437a779b13577a3583db312debe655e1d9cfc3b
Prince Oduro <prince@getinnotized.com>
2020-07-29 16:12:31 +0100

    withdrawals api added

---
+++ dev_resources/withdrawal.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.26-log : Database - kmadb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`kmadb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+
+USE `kmadb`;
+
+/*Table structure for table `withdrawal` */
+
+DROP TABLE IF EXISTS `withdrawal`;
+
+CREATE TABLE `withdrawal` (
+  `wid` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` decimal(10,2) DEFAULT NULL,
+  `name` varchar(90) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telephone` varchar(90) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `transactioncode` varchar(90) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `withdrwaldate` date DEFAULT NULL,
+  PRIMARY KEY (`wid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `withdrawal` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
